ecab45a486c0df54d074077835284da5744cd54e
Pooja <Pooja@LAPTOP-S9UKL0CR>
2022-08-11 20:37:51 +0530

    first commit

---
+++ Inheritance/src/com/tns/singletable/Manager.java
@@ -0,0 +1,25 @@
+package com.tns.singletable;
+
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("MGR")
+
+
+public class Manager extends Employee{
+	
+	private String DepartmentName;
+
+	public String getDepartmentName() {
+		return DepartmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		DepartmentName = departmentName;
+	}
+	
+ 
+
+}
